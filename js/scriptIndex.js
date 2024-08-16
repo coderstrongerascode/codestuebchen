b1db4dbdecdffdd287e0f702a13e17fe08ec3a3f
coderstrongerascode <Astronaut.zum.mars@gmail.com>
2024-08-16 05:07:40 +0000

    update indsex.html pageTitle

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -75,7 +75,7 @@ function loadLanguage() {
         fetch(`resources/language/${language}.json`)
             .then(response => response.json())
             .then(data => {
-                document.getElementById("headerTitle").textContent = data.headerTitle;
+                document.getElementById("pageTitle").textContent = data.pageTitle;
                 document.getElementById("languageLabel").textContent = data.languageLabel;
                 document.getElementById("homeLink").textContent = data.homeLink;
                 document.getElementById("diceLink").textContent = data.diceLink;
